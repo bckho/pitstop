a551db0712a5cda5f94c305551769fa53debfaca
Bryan K <bryan.cs@live.nl>
2021-05-16 17:22:03 +0200

    changed docker volume names to avoid conflicts

--- src/RebuildAllDockerImages.ps1
+++ src/RebuildAllDockerImages.ps1
@@ -1,8 +1,8 @@
 echo "====================="
 echo "====== Volumes ======"
 echo "====================="
-docker volume create --name=sqlserverdata
-docker volume create --name=rabbitmqdata
+docker volume create --name=pitstopsqlserverdata
+docker volume create --name=pitstoprabbitmqdata
 
 # Rebuild all the services that have changes
 # If you want to (re)build only a specific service, go to the src folder and execute `docker-compose build <servicename-lowercase>`
